e22e983635e24032774ae401fd4faf15c19434fe
Hussnainulabidin <hussnainulabidin707@gmail.com>
2025-04-25 06:42:30 +0500

    final ho gya sahid 2

--- Frontend/src/pages/league.jsx
+++ Frontend/src/pages/league.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LeagueComponent from '../components/Accounts/League/League';
 import LeagueDetail from '../components/Accounts/League/League-detail';
-import './league.css';
+import './League.css';
 
 function LeaguePage() {
   return (
