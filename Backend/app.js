338761238828149745a6ea6b540f5536c6e22475
Hussnainulabidin <hussnainulabidin707@gmail.com>
2025-05-04 02:21:14 +0500

    he

--- Backend/app.js
+++ Backend/app.js
@@ -34,9 +34,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-  origin: [
-    '*', 
-  ],
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
